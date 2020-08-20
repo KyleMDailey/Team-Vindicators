2eddd402240e36c6c99256541fd13f40eeb3b806
Kyle Dailey <67755613+KyleMDailey@users.noreply.github.com>
2020-08-20 08:27:57 -0400

    Add files via upload

---
+++ MilitaryUnitInheritance_DaileyKyle08182020/Program.cs
@@ -0,0 +1,68 @@
+﻿//Authors:  Kyle Dailey, Tyler Kain, Florabel Ituralde, Tim Tieng (Team Vindicators)
+//Date:  08/18/2020
+
+/**************************************************************************************************************************************************************************************
+
+Description:  This project contains code for the Military Unit Inheritance exercise.  It will create a console application which demonstrates inheritance.
+
+***************************************************************************************************************************************************************************************
+
+Revisions:  None
+
+Revision Date:  N/A
+
+Revised By:  N/A
+
+***************************************************************************************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryUnitInheritance_DaileyKyle08182020
+{
+    class Program
+    {
+        static void myTeamInfo()
+        {
+            Console.WriteLine("This is my Team!");
+            Team myAlphaTeam = new Team();
+            myAlphaTeam.rank = "SGT";
+            myAlphaTeam.position = "Rifleman";
+            myAlphaTeam.size = 4;
+            myAlphaTeam.alphaTeam();
+
+            Team myBravoTeam = new Team();// this is my squad
+            myBravoTeam.rank = "CPL";
+            myBravoTeam.position = "Grenadier";
+            myBravoTeam.size = 3;
+            myBravoTeam.bravoTeam();
+
+            Console.WriteLine("This is my Squad!");
+            Squad mySquad = new Squad();
+            mySquad.rank = "SGT";
+            mySquad.position = "Squad Leader";
+            mySquad.PSG();
+            mySquad.PLLeader();
+
+            //Console.WriteLine($"My squad carries {lightWeapons}.");
+            //Working to add a weapons interface.
+        }
+
+        static void Main()
+        {
+            try
+            {
+                myTeamInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("You did something wrong: {0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("AIRBORNE");
+            }
+        }
+    }
+}
---
+++ MilitaryUnitInheritance_DaileyKyle08182020/Weapons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryUnitInheritance_DaileyKyle08182020
+{
+    interface IWeapons
+        //Working to get an interface with weapons.
+    {
+        void Weapons();
+    }
+
+    //public class Weapons()
+    //{
+    //    //public string lightWeapons()// { get; set; }
+    //    //public string heavyWeapons { get; set; }
+    //    //public string indirectWeapons { get; set; }
+    //    //public int ammo { get; set; }
+
+    //    //public void weaponLoadout()
+    //    //{
+    //    //    lightWeapons = "M4, M4-203";
+    //    //    heavyWeapons = "M240B, M2";
+    //    //    indirectWeapons = "60mm Mortar";
+    //    //}
+    //}
+}
